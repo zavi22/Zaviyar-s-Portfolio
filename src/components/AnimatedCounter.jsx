64a17a2086d02d8601d35157988d4440f2a022c8
Zaviyar Butt <zaviyar55@gmail.com>
2026-03-10 21:25:42 +0500

    Making it responsive

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -4,7 +4,7 @@ import CountUp from 'react-countup'
 
 const AnimatedCounter = () => {
     return (
-        <div id='counter' className='padding-x-lg xl:mt-0 md:mt-90 mt-50'>
+        <div id='counter' className='padding-x-lg xl:mt-0 md:mt-90 mt-32'>
             <div className='mx-auto grid-4-cols'>
                 {counterItems.map((item, index) => {
                     return (
