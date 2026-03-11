c228c74627c051742a115d97d7467e0d6e262fd6
Zaviyar Butt <zaviyar55@gmail.com>
2026-03-11 10:23:01 +0500

    Corrections

--- src/constants/index.js
+++ src/constants/index.js
@@ -241,44 +241,44 @@ const expLogos = [
 const testimonials = [
   {
     name: "Esther Howard",
-    mentions: "@estherhoward",
+    // mentions: "@estherhoward",
     review:
-      "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+      "I can’t say enough good things about Zaviyar. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
     imgPath: "/images/client1.png",
   },
   {
     name: "Wade Warren",
-    mentions: "@wadewarren",
+    // mentions: "@wadewarren",
     review:
-      "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+      "Working with Zaviyar was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
     imgPath: "/images/client3.png",
   },
   {
     name: "Guy Hawkins",
-    mentions: "@guyhawkins",
+    // mentions: "@guyhawkins",
     review:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Zaviyar was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Zaviyar's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Zaviyar is the ideal partner.",
     imgPath: "/images/client2.png",
   },
   {
     name: "Marvin McKinney",
-    mentions: "@marvinmckinney",
+    // mentions: "@marvinmckinney",
     review:
-      "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+      "Zaviyar was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
     imgPath: "/images/client5.png",
   },
   {
     name: "Floyd Miles",
-    mentions: "@floydmiles",
+    // mentions: "@floydmiles",
     review:
-      "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+      "Zaviyar’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
     imgPath: "/images/client4.png",
   },
   {
     name: "Albert Flores",
-    mentions: "@albertflores",
+    // mentions: "@albertflores",
     review:
-      "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
+      "Zaviyar was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
     imgPath: "/images/client6.png",
   },
 ];
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -9,12 +9,12 @@ const Testimonials = () => {
             <div className='w-full h-full md:px-10 px-5'>
                 <Title title='What People Say About Me?' sub='⭐ Client Feedback Highlights' />
                 <div className='lg:columns-3 md:columns-2 columns-1 mt-16'>
-                    {testimonials.map(({ imgPath, name, review, mentions }) => (
+                    {testimonials.map(({ imgPath, name, review }) => (
                         <GlowCard key={name} card={{ review }}>
                             <div className='flex items-center gap-3'>
                                 <div><img src={imgPath} alt={name} /></div>
                                 <div><p className='font-bold'>{name}</p>
-                                    <p className='text-white-50'>{mentions}</p>
+                                    {/* <p className='text-white-50'>{mentions}</p> */}
                                 </div>
                             </div>
                         </GlowCard>
